bc8a9c7e47b58647e5056639f9eb0663d4be1086
Diaby Almamy <diabyissouf149@gmail.com>
2022-08-29 14:08:07 +0000

    models de l'app students ajoutés dans l'interface d'admin

--- src/students/admin.py
+++ src/students/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Student, Faculty, Speciality, Mark
+
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "last_name",
+        "first_name",
+        "faculty",
+        "speciality",
+    )
+
+
+@admin.register(Faculty)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ("name", )
+
+
+@admin.register(Speciality)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ("name", "faculty")
+
+
+@admin.register(Mark)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ("mark_type", "student")
+
+
--- src/students/models.py
+++ src/students/models.py
@@ -4,10 +4,22 @@ from django.db import models
 class Faculty(models.Model):
     name = models.CharField(max_length=200)
 
+    class Meta:
+        verbose_name = "facultée"
+
+    def __str__(self):
+        return f"{self.name}"
+
 
 class Speciality(models.Model):
     name = models.CharField(max_length=100)
     faculty = models.ForeignKey(Faculty, on_delete=models.SET_NULL, null=True)
+
+    class Meta:
+        verbose_name = "spécialitée"
+
+    def __str__(self):
+        return f"{self.name}({self.faculty})"
 
 
 class Student(models.Model):
@@ -19,8 +31,16 @@ class Student(models.Model):
     last_name = models.CharField(max_length=150, verbose_name="Nom")
     birth = models.DateField(verbose_name="Naissance", blank=True, null=True)
 
+    class Meta:
+        verbose_name = "étudiant"
+
+    def __str__(self):
+        return f"{self.last_name} {self.first_name}"
+
 
 class Mark(models.Model):
     mark_type = models.FloatField(verbose_name="Note")
     student = models.ForeignKey(Student, on_delete=models.CASCADE)
 
+    class Meta:
+        verbose_name = "note"
