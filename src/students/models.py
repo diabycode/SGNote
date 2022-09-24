76578ae29b66a07a307427cd96cdb2fb23ec23c5
Diaby Almamy <diabyissouf149@gmail.com>
2022-09-24 16:30:40 +0000

    students/models.py: model 'Student', ajout de quelques méthodes pour des calculs de moyennes et autres

--- src/students/models.py
+++ src/students/models.py
@@ -4,7 +4,7 @@ import django
 from django.db import models
 from django.utils.text import slugify
 
-from departements_and_modules.models import Faculty, Speciality
+from departements_and_modules.models import Faculty, Speciality, Lesson
 
 
 class Student(models.Model):
@@ -37,4 +37,71 @@ class Student(models.Model):
 
         return marks
 
+    def _get_lesson_class_mean(self, lesson, semester, academic_year):
+        lesson_class_mean = 0
+
+        marks = self.get_marks(lesson=lesson, semester=semester, academic_year=academic_year)
+        marks = marks.filter(is_exam=False)
+        if marks:
+            marks_int = [float(m.mark_type) for m in marks]
+            lesson_class_mean = sum(marks_int)/len(marks)
+        return lesson_class_mean
+
+    def _get_lesson_exam_mark(self, lesson, semester, academic_year, to_float=False):
+        marks = self.get_marks(lesson=lesson, semester=semester, academic_year=academic_year)
+        if marks:
+            exam_mark = marks.get(is_exam=True)
+            if exam_mark:
+                if to_float:
+                    exam_mark = float(exam_mark.mark_type)
+                return exam_mark
+        return 0
+
+    def _get_lesson_semester_mean(self, lesson, semester):
+        semester_mean = 0
+        class_mean = self._get_lesson_class_mean(lesson=lesson, semester=semester, academic_year=semester.academic_year)
+        exam_mean = self._get_lesson_exam_mark(to_float=True, lesson=lesson, semester=semester, academic_year=semester.academic_year)
+        semester_mean = (class_mean * 40 + exam_mean * 60) / 100
+        return semester_mean
+
+    def _get_semester_mean(self, semester):
+        modules = self.faculty.module_set.all()
+        lessons = []
+        for module in modules:
+            lessons.append(Lesson.objects.filter(module=module))
+
+        means_coefficiented = []
+        coefficient_count = .0
+        for lesson in lessons:
+            lesson_semester_mean = self._get_lesson_semester_mean(lesson=lesson, semester=semester)
+            lesson_semester_mean_coefficiented = lesson_semester_mean*float(lesson.coefficient)
+
+            means_coefficiented.append(lesson_semester_mean_coefficiented)
+            coefficient_count += float(lesson.coefficient)
+        semester_mean = sum(means_coefficiented)/coefficient_count
+        return semester_mean
+
+    def _get_annual_mean(self, academic_year):
+        semesters = academic_year.semester_set.all()
+        semesters_mean = []
+        for semester in semesters:
+            semester_mean = self._get_semester_mean(semester=semester)
+            semesters_mean.append(semester_mean)
+        annual_mean = sum(semesters_mean)/len(semesters)
+        return annual_mean
+
+    def get_class_mean(self, lesson, semester, academic_year):
+        return self._get_lesson_class_mean(lesson, semester, academic_year)
+
+    def get_lesson_exam_mark(self, lesson, semester, academic_year):
+        return self._get_lesson_exam_mark(lesson, semester, academic_year)
+
+    def get_lesson_semester_mean(self, lesson, semester):
+        return self._get_lesson_semester_mean(lesson, semester)
+
+    def get_semester_mean(self, semester):
+        return self._get_semester_mean(semester)
+
+    def get_annual_mean(self, academic_year):
+        return self._get_annual_mean(academic_year)
 
