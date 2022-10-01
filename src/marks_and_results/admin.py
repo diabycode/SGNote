74df3d6cedebffa63ab0230d872b560c6fe0d201
Diaby Almamy <diabyissouf149@gmail.com>
2022-10-01 12:21:28 +0000

    --

--- src/marks_and_results/admin.py
+++ src/marks_and_results/admin.py
@@ -4,7 +4,7 @@ from .models import Mark
 # Register your models here.
 
 
-@admin.register(Mark)
-class MarkAdmin(admin.ModelAdmin):
-    list_display = ("student", "lesson", "semester", "mark_type", )
-
+# @admin.register(Mark)
+# class MarkAdmin(admin.ModelAdmin):
+#     list_display = ("student", "lesson", "semester", "mark_type", )
+#
