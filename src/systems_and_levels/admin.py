71ab5678a1bd7859141ade930f904820d3c9348c
Diaby Almamy <diabyissouf149@gmail.com>
2022-10-01 11:02:46 +0000

    Ajout des models Sytem et Level dans l'interface d'admin

--- src/systems_and_levels/admin.py
+++ src/systems_and_levels/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from systems_and_levels.models import System, Level
+
+
+@admin.register(System)
+class SystemAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "name",
+    )
+
+
+@admin.register(Level)
+class LevelAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "name",
+    )
--- src/systems_and_levels/models.py
+++ src/systems_and_levels/models.py
@@ -4,9 +4,22 @@ from django.db import models
 class System(models.Model):
     name = models.CharField(max_length=80, verbose_name="Système")
 
+    class Meta:
+        verbose_name = "Système"
+
+    def __str__(self):
+        return self.name
+
 
 class Level(models.Model):
     name = models.CharField(max_length=80, verbose_name="Niveau")
     system = models.ForeignKey(System, on_delete=models.CASCADE, verbose_name="Système")
+
+    class Meta:
+        verbose_name = "Niveau"
+        verbose_name_plural = "Niveaux"
+
+    def __str__(self):
+        return self.name
 
 
