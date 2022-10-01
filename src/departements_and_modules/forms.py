3c2691a38c1797d359b473141f3f74fbeb3bb738
Diaby Almamy <diabyissouf149@gmail.com>
2022-10-01 11:28:08 +0000

    formulaire de saisi de Faculty, Speciality, Module, Lesson supprimés

--- src/departements_and_modules/forms.py
+++ src/departements_and_modules/forms.py
@@ -3,40 +3,40 @@ from django.forms import ModelForm
 from departements_and_modules.models import Faculty, Module, Speciality, Lesson
 
 
-class FacultyCreateForm(ModelForm):
+# class FacultyCreateForm(ModelForm):
+#
+#     class Meta:
+#         model = Faculty
+#         fields = [
+#             "name",
+#         ]
+#
 
-    class Meta:
-        model = Faculty
-        fields = [
-            "name",
-        ]
-
-
-class ModuleCreateForm(ModelForm):
-
-    class Meta:
-        model = Module
-        fields = [
-            "name",
-            "faculty",
-        ]
-
-
-class SpecialityCreateForm(ModelForm):
-
-    class Meta:
-        model = Speciality
-        fields = [
-            "name",
-            "faculty",
-        ]
+# class ModuleCreateForm(ModelForm):
+#
+#     class Meta:
+#         model = Module
+#         fields = [
+#             "name",
+#             "faculty",
+#         ]
 
 
-class LessonCreateForm(ModelForm):
-    class Meta:
-        model = Lesson
-        fields = [
-            "name",
-            "coefficient",
-            "module",
-        ]
+# class SpecialityCreateForm(ModelForm):
+#
+#     class Meta:
+#         model = Speciality
+#         fields = [
+#             "name",
+#             "faculty",
+#         ]
+#
+
+# class LessonCreateForm(ModelForm):
+#     class Meta:
+#         model = Lesson
+#         fields = [
+#             "name",
+#             "coefficient",
+#             "module",
+#         ]
