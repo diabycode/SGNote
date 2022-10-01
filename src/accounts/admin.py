93291db671c13e24a4013a38b72b37d050df1cb3
Diaby Almamy <diabyissouf149@gmail.com>
2022-10-01 10:55:24 +0000

    accounts/admin.py : Ajout de LogEntry dans l'interface d'admin pour la gestion des différents action de changements éffectué par chanque utilistaeurs

--- src/accounts/admin.py
+++ src/accounts/admin.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
+from django.contrib.admin.models import LogEntry
 
 from .models import CustomUser
 
 
+@admin.register(LogEntry)
+class LogEntryAdmin(admin.ModelAdmin):
+    list_display = ("action_flag", "object_repr", "user", "action_time", )
+
+
 @admin.register(CustomUser)
 class UserAdmin(admin.ModelAdmin):
-    pass
+    list_display = ("username", "first_name", "last_name", "email", "date_joined",)
+
+
 
