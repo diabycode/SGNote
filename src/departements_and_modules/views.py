6a3be0a11b84986ac24655e7fa9e43765bb980a8
Diaby Almamy <diabyissouf149@gmail.com>
2022-10-01 12:23:27 +0000

    departements_and_modules/views.py: traitements des formulaire supprimés

--- src/departements_and_modules/views.py
+++ src/departements_and_modules/views.py
@@ -1,64 +1,41 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-from .models import Faculty, Module, Lesson, Speciality
-from .forms import FacultyCreateForm, ModuleCreateForm, LessonCreateForm, SpecialityCreateForm
+from .models import Faculty, Module, Lesson, Speciality, AcademicYear
 
 
+@login_required()
 def students_faculties(request):
     context = {
         "faculties": Faculty.objects.all(),
-        "add_form": FacultyCreateForm,
+        "now_academic_year": AcademicYear.objects.get(is_now_academic_year=True)
     }
-
-    if request.method == "POST":
-        add_form = FacultyCreateForm(data=request.POST)
-        if add_form.is_valid():
-            add_form.save()
-        else:
-            context["add_form"] = FacultyCreateForm(initial={"name": request.POST["name"]})
     return render(request, "departements_and_modules/faculties.html", context=context)
 
 
+@login_required()
 def students_modules(request):
     context = {
         "modules": Module.objects.all(),
-        "add_form": ModuleCreateForm,
+        "now_academic_year": AcademicYear.objects.get(is_now_academic_year=True)
     }
-    if request.method == "POST":
-        add_form = ModuleCreateForm(data=request.POST)
-        if add_form.is_valid():
-            add_form.save()
-        else:
-            context["add_form"] = ModuleCreateForm(initial={"name": request.POST["name"]})
-
     return render(request, "departements_and_modules/modules.html", context=context)
 
 
+@login_required()
 def students_lessons(request):
     context = {
         "lessons": Lesson.objects.all(),
-        "add_form": LessonCreateForm,
+        "now_academic_year": AcademicYear.objects.get(is_now_academic_year=True)
     }
-    if request.method == "POST":
-        add_form = LessonCreateForm(data=request.POST)
-        if add_form.is_valid():
-            add_form.save()
-        else:
-            context["add_form"] = LessonCreateForm(initial={"name": request.POST["name"]})
-
     return render(request, "departements_and_modules/lessons.html", context=context)
 
 
+@login_required()
 def students_specialities(request):
     context = {
         "specialities": Speciality.objects.all(),
-        "add_form": SpecialityCreateForm,
+        "now_academic_year": AcademicYear.objects.get(is_now_academic_year=True)
     }
-    if request.method == "POST":
-        add_form = SpecialityCreateForm(data=request.POST)
-        if add_form.is_valid():
-            add_form.save()
-        else:
-            context["add_form"] = SpecialityCreateForm(initial={"name": request.POST["name"]})
     return render(request, "departements_and_modules/specialities.html", context=context)
 
