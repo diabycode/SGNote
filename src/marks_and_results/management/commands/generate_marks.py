3dedd78947694e9c40ef77d472d8aaf73de9bdb3
Diaby Almamy <diabyissouf149@gmail.com>
2022-10-01 12:16:37 +0000

    marks_and_results/management/commands/generate_marks.py: commande de generation revue

--- src/marks_and_results/management/commands/generate_marks.py
+++ src/marks_and_results/management/commands/generate_marks.py
@@ -23,30 +23,34 @@ class Command(BaseCommand):
             module_selected = modules[random.randint(0, len(modules)-1)]
 
             lessons = Lesson.objects.filter(module=module_selected)
+            lesson_selected = lessons[random.randint(0, len(lessons) - 1)]
 
             academic_year = AcademicYear.objects.last()
+
             semesters = academic_year.semester_set.all()
+            semester_selected = semesters[random.randint(0, len(semesters) - 1)]
 
             students = Student.objects.filter(faculty=faculty_selected)
-            for student in students:
-                print(student)
-                for i in range(number):
-                    semester_selected = semesters[random.randint(0, len(semesters) - 1)]
-                    lesson_selected = lessons[random.randint(0, len(lessons) - 1)]
+            if students:
+                for student in students:
+                    print(student)
+                    for i in range(number):
+                        mark = Mark()
+                        mark.student = student
+                        mark.lesson = lesson_selected
+                        mark.mark_type = random.randint(5, 19)
+                        mark.academic_year = academic_year
+                        mark.semester = semester_selected
 
-                    mark = Mark()
-                    mark.student = student
-                    mark.lesson = lesson_selected
-                    mark.mark_type = random.randint(5, 19)
-                    mark.academic_year = academic_year
-                    mark.semester = semester_selected
-
-                    mark.save()
-                    print(f"{mark.mark_type} / {mark.lesson} / {mark.semester} / {mark.academic_year}")
-                print("---------------------")
-            print()
-            print(f"{number} marks generated")
-            print(f"faculty: {faculty_selected}")
-            print(f"module: {module_selected}")
-
+                        mark.save()
+                        print(f"{mark.mark_type} / {mark.lesson} / {mark.semester} / {mark.academic_year}")
+                    print("---------------------")
+                print()
+                print(f"{number} marks generated")
+                print(f"faculty: {faculty_selected}")
+                print(f"module: {module_selected}")
+                print(f"lesson: {lesson_selected}")
+                print(f"semester: {semester_selected}")
+            else:
+                print("Aucun étudiant")
 
