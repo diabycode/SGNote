ec9aad9a054ecc8fc02510e15ca186a96ce9649a
Diaby Almamy <diabyissouf149@gmail.com>
2022-10-01 10:57:17 +0000

    students/management/commands/generate_students.py : revue de la commande de génération d'étudiants

--- src/students/management/commands/generate_students.py
+++ src/students/management/commands/generate_students.py
@@ -28,32 +28,30 @@ class Command(BaseCommand):
             fk = faker.Faker("fr-FR")
 
             faculties = Faculty.objects.all()
+            faculty_selected = faculties[random.randint(0, len(faculties) - 1)]
+
+            specialities = Speciality.objects.filter(faculty=faculty_selected)
+            speciality_selected = specialities[random.randint(0, len(specialities) - 1)]
 
             for i in range(number):
                 student = Student()
-                specialities = Speciality.objects.all()
+                student.faculty = faculty_selected
+                student.speciality = speciality_selected
 
                 student.last_name = fk.last_name()
                 student.first_name = fk.first_name()
                 student.matricule = get_str(10).upper()
                 student.birth = None
 
-                student.faculty = faculties[random.randint(0, len(faculties)-1)]
-
-                specialities = specialities.filter(faculty=student.faculty)
-                if specialities:
-                    student.speciality = specialities[random.randint(0, len(specialities)-1)]
-
                 student.save()
 
                 print(student.first_name)
                 print(student.last_name)
                 print(student.matricule)
-                print(student.faculty)
-                print(student.speciality)
-                print(student.birth)
                 print("-"*10)
 
             print()
             print(f"{number} Student(s) created")
+            print(f"Faculty: {faculty_selected}")
+            print(f"Speciality: {speciality_selected}")
 
