0327ee7fb2e9cdb37022e20e98a33df2ece78a59
Diaby Almamy <diabyissouf149@gmail.com>
2022-10-13 21:10:54 +0000

    --

--- src/marks_and_results/admin.py
+++ src/marks_and_results/admin.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
 
 from .models import Mark
-# Register your models here.
 
 
-# @admin.register(Mark)
-# class MarkAdmin(admin.ModelAdmin):
-#     list_display = ("student", "lesson", "semester", "mark_type", )
-#
+@admin.register(Mark)
+class MarkAdmin(admin.ModelAdmin):
+    list_display = ("student", "lesson", "semester", "mark_type", )
+
