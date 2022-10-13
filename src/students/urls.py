6e2262ec9e86137611174c9e9d22c65a590012e6
Diaby Almamy <diabyissouf149@gmail.com>
2022-10-13 21:03:28 +0000

    Ajout de la vue de modif d'un étudiant

--- src/students/urls.py
+++ src/students/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('list/', students_list, name="students_list"),
     path('details/<str:pk>/', students_details, name="students_details"),
     path('create/', student_create_view, name="student_create"),
+    path('edit/<str:pk>/', student_edit, name="student_edit"),
 
     path('specialities_dropdown/', specialities_dropdown, name="specialities_dropdown"),
     path('level_dropdown/', level_dropdown, name="level_dropdown"),
