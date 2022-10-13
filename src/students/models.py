5b1ce778717c075a805ecc5f31c82da5d8bb9cf8
Diaby Almamy <diabyissouf149@gmail.com>
2022-10-13 20:46:43 +0000

    model Student: méthodes de calcul revus

--- src/students/models.py
+++ src/students/models.py
@@ -16,6 +16,7 @@ class Student(models.Model):
     matricule = models.CharField(max_length=255, primary_key=True, unique=True, verbose_name="Matricule")
     first_name = models.CharField(max_length=150, verbose_name="Prénom")
     last_name = models.CharField(max_length=150, verbose_name="Nom")
+    gender = models.CharField(max_length=20, verbose_name="Genre", null=True)
     birth = models.DateField(verbose_name="Naissance", blank=True, null=True)
 
     class Meta:
@@ -28,79 +29,67 @@ class Student(models.Model):
     def full_name(self):
         return f"{self.last_name} {self.first_name}"
 
-    def get_marks(self, lesson=None, semester=None, academic_year=None):
+    def _get_marks(self, lesson=None, semester=None, academic_year=None, exclude_exam=False):
         marks = self.mark_set.all()
         if lesson:
             marks = marks.filter(lesson=lesson)
-        if semester:
-            marks = marks.filter(semester=semester)
         if academic_year:
             marks = marks.filter(academic_year=academic_year)
-
+        if semester:
+            marks = marks.filter(semester=semester)
+        if exclude_exam:
+            marks = marks.filter(is_exam=False)
         return marks
 
-    def _get_lesson_class_mean(self, lesson, semester, academic_year):
-        lesson_class_mean = 0
+    def _get_a_lesson_class_mean(self, lesson, semester):
+        """ get student marks mean in a specific lesson and return it """
 
-        marks = self.get_marks(lesson=lesson, semester=semester, academic_year=academic_year)
-        marks = marks.filter(is_exam=False)
+        mean = None
+        marks = self._get_marks(lesson=lesson, semester=semester, exclude_exam=True)
         if marks:
-            marks_int = [float(m.mark_type) for m in marks]
-            lesson_class_mean = sum(marks_int)/len(marks)
-        return lesson_class_mean
+            marks_total = 0
+            for mark in marks:
+                marks_total += float(mark.mark_type)
+            mean = marks_total/len(marks)
+        return mean
 
-    def _get_lesson_exam_mark(self, lesson, semester, academic_year, to_float=False):
-        marks = self.get_marks(lesson=lesson, semester=semester, academic_year=academic_year)
+    def _get_a_lesson_exam_mark(self, lesson, semester, to_float=False):
+        """ get student exam mark in a specific lesson and return it """
+
+        exam_mark = None
+        marks = self._get_marks(lesson=lesson, semester=semester)
         if marks:
-            exam_mark = marks.filter(is_exam=True)
-            if exam_mark:
-                exam_mark = exam_mark[0]
-                if to_float:
-                    exam_mark = float(exam_mark.mark_type)
-                return exam_mark
-        return 0
+            exam_mark = marks.get(is_exam=True) if marks.filter(is_exam=True) else None
+            if exam_mark is not None and to_float:
+                exam_mark = float(exam_mark.mark_type)
+        return exam_mark
 
-    def _get_lesson_semester_mean(self, lesson, semester):
-        semester_mean = 0
-        class_mean = self._get_lesson_class_mean(lesson=lesson, semester=semester, academic_year=semester.academic_year)
-        exam_mean = self._get_lesson_exam_mark(to_float=True, lesson=lesson, semester=semester, academic_year=semester.academic_year)
-        semester_mean = (class_mean * 40 + exam_mean * 60) / 100
+    def _get_a_lesson_semester_mean(self, lesson, semester):
+        semester_mean = None
+        class_mean = self._get_a_lesson_class_mean(lesson=lesson, semester=semester)
+        if class_mean is not None:
+            exam_mark = self._get_a_lesson_exam_mark(to_float=True, lesson=lesson, semester=semester)
+            if exam_mark is not None:
+                semester_mean = (class_mean * 40 + exam_mark * 60) / 100
         return semester_mean
 
     def _get_semester_mean(self, semester):
-        modules = self.faculty.module_set.all()
-        lessons = []
-        for module in modules:
-            lessons.append(Lesson.objects.filter(module=module))
-
-        means_coefficiented = []
-        coefficient_count = .0
-        for lesson in lessons:
-            lesson_semester_mean = self._get_lesson_semester_mean(lesson=lesson, semester=semester)
-            lesson_semester_mean_coefficiented = lesson_semester_mean*float(lesson.coefficient)
-
-            means_coefficiented.append(lesson_semester_mean_coefficiented)
-            coefficient_count += float(lesson.coefficient)
-        semester_mean = sum(means_coefficiented)/coefficient_count
-        return semester_mean
+        pass
 
     def _get_annual_mean(self, academic_year):
-        semesters = academic_year.semester_set.all()
-        semesters_mean = []
-        for semester in semesters:
-            semester_mean = self._get_semester_mean(semester=semester)
-            semesters_mean.append(semester_mean)
-        annual_mean = sum(semesters_mean)/len(semesters)
-        return annual_mean
+        pass
 
-    def get_class_mean(self, lesson, semester, academic_year):
-        return self._get_lesson_class_mean(lesson, semester, academic_year)
+    def get_marks(self, lesson=None, semester=None, academic_year=None):
+        return self._get_marks(lesson=lesson, semester=semester, academic_year=academic_year)
 
-    def get_lesson_exam_mark(self, lesson, semester, academic_year):
-        return self._get_lesson_exam_mark(lesson, semester, academic_year)
+    def get_a_lesson_class_mean(self, lesson, semester):
+        return self._get_a_lesson_class_mean(lesson, semester)
 
-    def get_lesson_semester_mean(self, lesson, semester):
-        return self._get_lesson_semester_mean(lesson, semester)
+    def get_a_lesson_exam_mark(self, lesson, semester):
+        return self._get_a_lesson_exam_mark(lesson=lesson, semester=semester)
+
+    def get_a_lesson_semester_mean(self, lesson, semester):
+        return self._get_a_lesson_semester_mean(lesson, semester)
 
     def get_semester_mean(self, semester):
         return self._get_semester_mean(semester)
